f86421d46e24188561ee79ad53561d790e0b0bfa
AbelG9 <55357845+AbelG9@users.noreply.github.com>
2023-12-20 11:39:18 -0500

    created services to implement ports

---
+++ src/main/java/com/codigo/semana7/application/usercase/PersonaServiceImpl.java
@@ -0,0 +1,35 @@
+package com.codigo.semana7.application.usercase;
+
+import com.codigo.semana7.domain.model.Persona;
+import com.codigo.semana7.domain.ports.in.PersonaIn;
+import com.codigo.semana7.domain.ports.out.PersonaOut;
+
+import java.util.Optional;
+
+public class PersonaServiceImpl implements PersonaIn {
+    private final PersonaOut personaOut;
+
+    public PersonaServiceImpl(PersonaOut personaOut) {
+        this.personaOut = personaOut;
+    }
+
+    @Override
+    public Persona crearPersona(Persona persona) {
+        return personaOut.createPersona(persona);
+    }
+
+    @Override
+    public Optional<Persona> getPersona(Long id) {
+        return personaOut.getPersona(id);
+    }
+
+    @Override
+    public Optional<Persona> actualizarPersona(Long id, Persona persona) {
+        return personaOut.updatePersona(id, persona);
+    }
+
+    @Override
+    public boolean borrarPersona(Long id) {
+        return personaOut.deletePersona(id);
+    }
+}
