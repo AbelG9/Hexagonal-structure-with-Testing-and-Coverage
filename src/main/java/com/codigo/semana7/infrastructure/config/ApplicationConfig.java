b3d8f98d103fc69f816f84b3cb2332b3d3be038f
AbelG9 <55357845+AbelG9@users.noreply.github.com>
2023-12-20 11:41:01 -0500

    created config to help springboot to recognize beans

---
+++ src/main/java/com/codigo/semana7/infrastructure/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package com.codigo.semana7.infrastructure.config;
+
+import com.codigo.semana7.application.service.PersonaService;
+import com.codigo.semana7.application.usercase.PersonaServiceImpl;
+import com.codigo.semana7.domain.ports.out.PersonaOut;
+import com.codigo.semana7.infrastructure.repository.PersonaJPARepositoryAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public PersonaService personaService(PersonaOut personaOut){
+        return new PersonaService(new PersonaServiceImpl(personaOut));
+    }
+
+    @Bean
+    public PersonaOut personaOut(PersonaJPARepositoryAdapter personaJPARepositoryAdapter) {
+        return personaJPARepositoryAdapter;
+    }
+}
