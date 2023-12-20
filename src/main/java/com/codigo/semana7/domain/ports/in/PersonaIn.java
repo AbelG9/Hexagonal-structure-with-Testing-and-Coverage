7bdfac390d8cd5eed4066e5046c56bb7125fdc28
AbelG9 <55357845+AbelG9@users.noreply.github.com>
2023-12-20 11:38:02 -0500

    created in and out ports

---
+++ src/main/java/com/codigo/semana7/domain/ports/in/PersonaIn.java
@@ -0,0 +1,12 @@
+package com.codigo.semana7.domain.ports.in;
+
+import com.codigo.semana7.domain.model.Persona;
+
+import java.util.Optional;
+
+public interface PersonaIn {
+    Persona crearPersona(Persona persona);
+    Optional<Persona> getPersona(Long id);
+    Optional<Persona> actualizarPersona(Long id, Persona persona);
+    boolean borrarPersona(Long id);
+}
